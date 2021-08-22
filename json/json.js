5b7ea1d2e90886247da04c4fd282216c469b9023
jungjuyoung <nwwn1004@naver.com>
2021-08-23 01:37:49 +0900

    about JSON

--- json/json.js
+++ json/json.js
@@ -1,7 +1,7 @@
 // JSON
 // Javscript Object Notation
 
-// Object to JOSN
+// Object to JOSN (오브젝트를 json으로 변환하는 방법)
 let json = JSON.stringify(true);
 let arr = ['apple', 'banana'];
 json = JSON.stringify(arr);
@@ -12,9 +12,10 @@ const rabbit = {
   size: null,
   participants: [{ name: 'namejoon' }, { name: 'jungkook' }],
   birthDate: new Date(),
-  jump: () => {
+  jump: () => { // 이건stringify하면 제외됨. 오브젝트에 있는 데이터가 아니라서 포함되지 않음.
     console.log(`${name} can jump!`);
   },
+  symbol: Symbol('id') // 자바스크립트의 특별한 데이터인 Symbol도 변환되지 않음.
 };
 json = JSON.stringify(rabbit);
 // JSON을 통제하고 싶을때 원하는 결과만 추출할때
@@ -26,11 +27,12 @@ json = JSON.stringify(rabbit, (key, value) => {
   // return key === 'name' ? 'jungkkok' : value;
 });
 
-// JSON to String
+// JSON to String (json을 object로 변환하는 방법)
 // console.clear();
+const json = JSON.stringify(rabbit)
 const obj = JSON.parse(json, (key, value) => {
   // console.log(`key: ${key}, value: ${value}`);
   return key === 'birthDate' ? new Date(value) : value;
 });
 
-// console.log(obj.birthDate.getDate());
+console.log(obj.birthDate.getDate());
